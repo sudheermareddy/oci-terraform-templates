5808028986c30bcc6a9e84c4eb4aea804fac3075
Ashwin Sebastian <asebastian@sysgain.com>
2017-09-22 15:41:09 +0530

    Delete vcn.tf

--- vcn.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_core_virtual_network" "vcn1" {
-  cidr_block = "10.1.0.0/16"
-  dns_label = "vcnash${random_id.unq.hex}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "vcnash${random_id.unq.hex}"
-}
