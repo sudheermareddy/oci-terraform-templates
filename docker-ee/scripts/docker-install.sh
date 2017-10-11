76f4b376ad75dcda7268b4fa4e87f67d66b8ea6a
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-11 16:17:19 +0530

    Update docker-install.sh

--- docker-ee/scripts/docker-install.sh
+++ docker-ee/scripts/docker-install.sh
@@ -63,5 +63,5 @@ sudo apt-get -y update
 sudo apt-get -y install docker-ee
 sudo apt-get -y update
 sudo usermod -aG docker $username
-echo -e "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
+echo -e "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
