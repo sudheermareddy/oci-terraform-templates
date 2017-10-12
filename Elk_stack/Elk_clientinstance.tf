9d8934403bf13aad7f66ce6a2309059254cefd40
Komali Oruganti <komalio@Sysgain.com>
2017-10-12 23:36:11 +0530

    up

---
+++ Elk_stack/Elk_clientinstance.tf
@@ -0,0 +1,21 @@
+resource "oci_core_instance" "Clientvm" {
+    availability_domain = "${lookup(data.oci_identity_availability_domains.availdomain.availability_domains[0],"name")}"
+    compartment_id = "${var.compartment_id}"
+    display_name = "${var.VCN-DisplayName}-client-instance${random_id.uniqueString.hex}"
+    image = "${lookup(data.oci_core_images.OLImageOCID.images[0], "id")}"
+    shape = "${var.InstanceShape}"
+    subnet_id = "${oci_core_subnet.Elksubnet1.id}"
+  metadata {
+        ssh_authorized_keys = "${var.ssh_public_key}"
+
+   }
+ create_vnic_details {
+    subnet_id = "${oci_core_subnet.Elksubnet1.id}"
+    display_name = "clientnic"
+    assign_public_ip = true
+    hostname_label = "clientinstance"
+  }
+    }
+output "Clientvmpublicip" {
+  value = ["${oci_core_instance.Clientvm.public_ip}"]
+}
