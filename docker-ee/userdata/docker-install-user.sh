c87e4166cad8d3ed913740e53f04c1e364986ee7
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-13 15:58:26 +0530

    Delete docker-install-user.sh

--- docker-ee/userdata/docker-install-user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~ubuntu
-
-curl https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-docker-ee/docker-ee/scripts/docker-install.sh > docker-install.sh
-
-chmod +x docker-install.sh
