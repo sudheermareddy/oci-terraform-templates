3c7aa2dcbfc9cec45c5814424d1cb592ff3fd825
ooha-m <omanduri@sysgain.com>
2017-10-13 11:51:29 +0530

    Delete customimage.tf

--- customimage.tf
+++
@@ -1,8 +0,0 @@
-resource "oci_core_image" "chefCustomImage" {
-    compartment_id = "${var.compId}"
-    display_name = "${var.vcnDisplayName}-CustomImage${random_id.unq.hex}"
-    instance_id = "${oci_core_instance.chefVM.id}"
-    timeouts {
-    create = "30m"
-   }
-}
