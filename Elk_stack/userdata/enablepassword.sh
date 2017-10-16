2cc0fbe582af2bfbe8cd8168ce120f4fd4936979
Komali Oruganti <komalio@Sysgain.com>
2017-10-16 22:16:34 +0530

    up

---
+++ Elk_stack/userdata/enablepassword.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd ~ubuntu
+
+curl https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-elk-stack/Elk_stack/userdata/enablepasswordelkvm.sh > enablepasswordelkvm.sh
+
+chmod +x enablepasswordelkvm.sh
