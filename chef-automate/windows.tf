8bd11fdc12026b7184791241615495b7c63ceed6
ooha-m <omanduri@sysgain.com>
2017-10-16 18:56:18 +0530

    Update windows.tf

--- chef-automate/windows.tf
+++ chef-automate/windows.tf
@@ -1,10 +1,27 @@
 resource "oci_core_instance" "windowsInstance" {
-  availability_domain = "${data.oci_identity_availability_domains.chefAD.availability_domains.1.name}"
+depends_on = ["oci_core_instance.chefVM-server"]
+  availability_domain = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[1],"name")}"
   compartment_id = "${var.compId}"
   display_name = "ooha-chefWindowsServer"
-  image = "${lookup(data.oci_core_images.wOLImageOCID.images[0], "id")}"
+  image = "${lookup(data.oci_core_images.wImageOCID.images[0], "id")}"
   shape = "${var.InstanceShape}"
   subnet_id = "${oci_core_subnet.chefsubnet1.id}"
   hostname_label = "winmachine"
   metadata {}
+}
+resource "null_resource" "remote-exec1" {
+  depends_on = ["oci_core_instance.windowsInstance"]
+    provisioner "remote-exec" {
+     connection {
+        agent = false
+       timeout = "15m"
+       host = "${data.oci_core_vnic.InstanceVnic.public_ip_address}"
+     }
+     inline = [
+       "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force",
+       "Invoke-WebRequest -Uri https://raw.githubusercontent.com/sudheermareddy/test/master/ws1.ps1  -OutFile c:/users/ws1.ps1",
+       "cd c:/users/"
+       "./ws1.ps1"
+     ]
+   }
 }
