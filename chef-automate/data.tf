1e2259c55ebe9fb38def4f3b7172d398e14eadbd
ooha-m <omanduri@sysgain.com>
2017-10-16 18:57:09 +0530

    Update data.tf

--- chef-automate/data.tf
+++ chef-automate/data.tf
@@ -16,3 +16,11 @@ data "oci_core_images" "wImageOCID" {
   operating_system = "${var.wInstanceOS}"
   operating_system_version = "${var.wInstanceOSVersion}"
 }
+data "oci_core_vnic_attachments" "InstanceVnicas" {
+    compartment_id = "${var.compId}" 
+    availability_domain = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[0],"name")}"
+    instance_id = "${oci_core_instance.chefVM-server.id}"
+} 
+data "oci_core_vnic" "InstanceVnic" {
+    vnic_id = "${lookup(data.oci_core_vnic_attachments.InstanceVnicas.vnic_attachments[0],"vnic_id")}"
+}
