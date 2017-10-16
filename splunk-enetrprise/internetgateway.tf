c7d71345632755748d37e8941e7c4a810563d3d4
sushmithan <sushmithan@sysgain.com>
2017-10-16 17:19:28 +0530

    Add files via upload

---
+++ splunk-enetrprise/internetgateway.tf
@@ -0,0 +1,6 @@
+
+resource "oci_core_internet_gateway" "internetgateway" {
+    compartment_id = "${var.COMP-ID}"
+    display_name = "${var.VCN-DisplayName}-igw${random_id.uniqueString.hex}"
+    vcn_id = "${oci_core_virtual_network.VCN.id}"
+}
