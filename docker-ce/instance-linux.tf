8176e4542d8087e24ba2a000f954a90661ea0dc5
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-17 20:41:24 +0530

    Update instance-linux.tf

--- docker-ce/instance-linux.tf
+++ docker-ce/instance-linux.tf
@@ -45,27 +45,3 @@ resource "oci_core_instance" "linux-b" {
         ))}"
   }
 }
-
-resource "oci_core_instance" "linux-c" {
-  count = "${var.instance_count}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[2],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "${var.prefix}-manager-${format("%02d", count.index + 1)}-c"
-  image = "${lookup(data.oci_core_images.LinImageOCID.images[0], "id")}"
-  shape = "${var.instanceShape}"
-  create_vnic_details {
-    subnet_id = "${oci_core_subnet.sub3.id}"
-    display_name = "${var.prefix}-man-${format("%02d", count.index + 1)}-nic-c"
-    assign_public_ip = true
-    hostname_label = "${var.prefix}-man-${format("%02d", count.index + 1)}-c"
-  },
-  metadata {
-    ssh_authorized_keys = "${var.sshPublicKey}"
-    user_data = "${base64encode(format("%s\n%s %s %s\n",
-           file(var.BootStrapFileDir),
-           "./enable-password-auth.sh",
-           "${var.admin_username}",
-           "${var.admin_password}"
-        ))}"
-  }
-}
