aea89f29b316d171e54341075d9d030bb63d9214
sushmithan <sushmithan@sysgain.com>
2017-10-17 10:54:14 +0530

    Update data.tf

--- splunk-enetrprise/data.tf
+++ splunk-enetrprise/data.tf
@@ -7,3 +7,13 @@ data "oci_core_images" "ImageOCID" {
     operating_system ="${var.InstanceOS}"
     operating_system_version =  "${var.InstanceOSVersion}"
 }
+
+data "oci_core_vnic_attachments" "splunkNic" {
+  compartment_id = "${var.COMP-ID}" 
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availdomain.availability_domains[0],"name")}"
+  instance_id = "${oci_core_instance.splunkvm.id}"
+} 
+
+data "oci_core_vnic" "splunk-nic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.splunkNic.vnic_attachments[0],"vnic_id")}"
+}
