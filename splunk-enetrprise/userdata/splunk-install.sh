19c87590ea83bd32809765158e2bb95d874da492
sushmithan <sushmithan@sysgain.com>
2017-10-17 16:43:27 +0530

    Update splunk-install.sh

--- splunk-enetrprise/userdata/splunk-install.sh
+++ splunk-enetrprise/userdata/splunk-install.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
-#This script is installing Splunk Enterprise
- 
-sudo wget -O splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.0&product=splunk&filename=splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz&wget=true'
-sudo tar xvzf splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz
+wget -O splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.0&product=splunk&filename=splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz&wget=true'
+tar xvzf splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz
 sudo mv splunk /opt/
-sudo rm splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz
-sudo /opt/splunk/bin/splunk start --accept-license
-sudo /opt/splunk/bin/splunk enable boot-start
- #sudo /opt/splunk/bin/splunk enable listen 9997
- #sudo /opt/splunk/bin/splunk restart
+rm splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz
+cd /opt/splunk/bin
+./splunk start --accept-license
+sudo ./splunk enable boot-start
+./splunk enable listen 9997 -auth admin:changeme
+./splunk restart
  
 #commands to open ports
 sudo apt install -y firewalld
