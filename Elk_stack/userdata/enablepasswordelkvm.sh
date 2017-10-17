b429c3922d75385a1a9e0a48f48f165a98356504
komali <komalio@sysgain.com>
2017-10-17 18:27:12 +0530

    Update enablepasswordelkvm.sh

--- Elk_stack/userdata/enablepasswordelkvm.sh
+++ Elk_stack/userdata/enablepasswordelkvm.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
-
-username=$1
-password=$2
-
+#!/bin/bash
+username="elastic"
+pwd="Password@1234"
 echo -e "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo -e "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 sudo usermod -l $username ubuntu
 usermod -d /home/$username -m $username
 
-echo -e "$password\n$password" | sudo passwd $username
+echo -e "$pwd\n$pwd" | sudo passwd $username
 file="/etc/ssh/sshd_config"
 passwd_auth="yes"
 cat $file \
