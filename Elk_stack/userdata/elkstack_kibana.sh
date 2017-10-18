e347b356096b2f80c335fb7f49c22af41aedbb2a
Komali Oruganti <komalio@Sysgain.com>
2017-10-18 13:11:13 +0530

    up

--- Elk_stack/userdata/elkstack_kibana.sh
+++ Elk_stack/userdata/elkstack_kibana.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository -y ppa:webupd8team/java >> $LOG
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - >> $LOG
 #echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list >> $LOG
 #echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
-#echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
+echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
 #echo "---Configure Repos for Azure Cli 2.0---" >> $LOG
 #echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list >> $LOG
 #sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 >> $LOG
