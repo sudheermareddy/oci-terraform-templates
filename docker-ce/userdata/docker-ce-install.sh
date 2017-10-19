36055e627ca6fde62a49400ef8649435cf188447
Ashwin Sebastian <asebastian@sysgain.com>
2017-10-19 22:10:07 +0530

    Update docker-ce-install.sh

--- docker-ce/userdata/docker-ce-install.sh
+++ docker-ce/userdata/docker-ce-install.sh
@@ -16,6 +16,7 @@ sudo firewall-cmd --add-port=443/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=4789/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=7946/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=2377/tcp --permanent --zone=public
+sudo firewall-cmd --add-port=5432/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=5000/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=5001/tcp --permanent --zone=public
 sudo firewall-cmd --add-port=8080/tcp --permanent --zone=public
