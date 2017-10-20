e0e25c09320232cc73fa86283bb732ab40eecb96
sushmithan <sushmithan@sysgain.com>
2017-10-20 11:24:14 +0530

    Update enablepassword-user.sh

--- splunk-enetrprise/userdata/enablepassword-user.sh
+++ splunk-enetrprise/userdata/enablepassword-user.sh
@@ -4,4 +4,4 @@ cd ~ubuntu
 
 curl https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-splunk-enterprise/splunk-enetrprise/userdata/enable-password.sh > enable-password.sh
 
-chmod +x enable-password-auth.sh
+chmod +x enable-password.sh
