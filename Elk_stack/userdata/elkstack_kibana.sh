7e17badc0fddc701a4547f46dee4e9556ef49ba0
komali <komalio@sysgain.com>
2017-10-20 10:28:13 +0530

    Update elkstack_kibana.sh

--- Elk_stack/userdata/elkstack_kibana.sh
+++ Elk_stack/userdata/elkstack_kibana.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
-#Date - 06102017
+#Date - 16102017
 #Developer - Sysgain
 
 DATE=`date +%Y%m%d%T`
 LOG=/tmp/elkstack_deploy.log.$DATE
 HOSTIP=`hostname -i`
-storageAccount=$1
-
 
 # Configure Repos for Java, Elasticsearch, Kibana Packages
-echo "---Configure Repos for Java, Elasticsearch, Kibana Packages---"	>> $LOG
+echo "---Configure Repos flasticsearchor Java, E, Kibana Packages---"   >> $LOG
 sudo add-apt-repository -y ppa:webupd8team/java >> $LOG
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - >> $LOG
-echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list >> $LOG
-echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
-echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
-echo "---Configure Repos for Azure Cli 2.0---" >> $LOG
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list >> $LOG
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 >> $LOG
+#echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list >> $LOG
+#echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
+#echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
+#echo "---Configure Repos for Azure Cli 2.0---" >> $LOG
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list >> $LOG
+#sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 >> $LOG
 
 # Enable silent installation
-echo "---Enable silent installation---"	>> $LOG
+echo "---Enable silent installation---" >> $LOG
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 
-# Repository Updates 
-echo "---Repository Updates---"	>> $LOG
+# Repository Updates
+echo "---Repository Updates---" >> $LOG
 sudo apt-get update
 
 #Installing Packages for ELK Stack
-echo "---Installing Packages for ELK Stack---"	>> $LOG
-sudo apt-get -y install oracle-java8-installer elasticsearch kibana nginx logstash unzip apt-transport-https azure-cli >> $LOG
+echo "---Installing Packages for ELK Stack---"  >> $LOG
+sudo apt-get -y install oracle-java8-installer nginx >> $LOG
 
 #Configuring Elasticsearch
 echo "---Configuring Elasticsearch---" >> $LOG
-sudo sed -i 's/# network.host: 192.168.0.1/ network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml >> $LOG
+sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.2.deb
+sudo dpkg -i elasticsearch-5.6.2.deb
+sudo sed -i 's/#network.host: 192.168.0.1/ network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml >> $LOG
+#sed -i 's/# network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml >> $LOG
 sudo systemctl restart elasticsearch >> $LOG
 sudo systemctl daemon-reload >> $LOG
-sudo systemctl enable elasticsearch >> $LOG 
+sudo systemctl enable elasticsearch >> $LOG
 
 #Configuring Kibana
 echo "---Configuring Kibana---" >> $LOG
-sudo sed -i 's/# server.host: "0.0.0.0"/ server.host: "localhost"/g' /opt/kibana/config/kibana.yml >> $LOG
+sudo wget https://artifacts.elastic.co/downloads/kibana/kibana-5.6.2-amd64.deb
+sudo dpkg -i kibana-5.6.2-amd64.deb
+sudo sed -i 's/#server.host: "localhost"/ server.host: "localhost"/g' /etc/kibana/kibana.yml
+#sudo sed -i 's/# server.host: "0.0.0.0"/ server.host: "localhost"/g' /opt/kibana/config/kibana.yml >> $LOG
 sudo systemctl daemon-reload >> $LOG
 sudo systemctl enable kibana >> $LOG
 sudo systemctl start kibana >> $LOG
@@ -53,6 +57,7 @@ echo "adminuser:`openssl passwd -apr1 'Password@1234'`" | sudo tee -a /etc/nginx
 sudo rm /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/sudheermareddy/test/master/default -O /etc/nginx/sites-available/default
 sudo sed -i 's/example.com/localhost:5601/g' /etc/nginx/sites-available/default
+#sudo curl https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-elk-stack/Elk_stack/scripts/default > /etc/nginx/sites-available/default >> $LOG
 sudo nginx -t >> $LOG
 sudo systemctl restart nginx >> $LOG
 sudo ufw allow 'Nginx Full' >> $LOG
@@ -61,16 +66,20 @@ sudo ufw allow 'Nginx Full' >> $LOG
 echo "---Generate SSL Certificates---" >> $LOG
 sudo mkdir -p /etc/pki/tls/certs >> $LOG
 sudo mkdir /etc/pki/tls/private >> $LOG
-sed -i "/\[ v3_ca \]/a subjectAltName = IP: $HOSTIP" /etc/ssl/openssl.cnf >> $LOG
+sudo sed -i "/\[ v3_ca \]/a subjectAltName = IP: $HOSTIP" /etc/ssl/openssl.cnf >> $LOG
 cd /etc/pki/tls >> $LOG
 sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt >> $LOG
 
+
 #Configuring Logstash
 echo "---Configuring Logstash---" >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/02-beats-input.conf -O /etc/logstash/conf.d/02-beats-input.conf >> $LOG
+cd ~
+sudo wget https://artifacts.elastic.co/downloads/logstash/logstash-5.6.2.deb
+sudo dpkg -i logstash-5.6.2.deb
+sudo wget https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-elk-stack/Elk_stack/scripts/02-beats-input.conf -O /etc/logstash/conf.d/02-beats-input.conf >> $LOG
 sudo ufw allow 5044 >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/10-syslog-filter.conf -O /etc/logstash/conf.d/10-syslog-filter.conf >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/30-elasticsearch-output.conf -O /etc/logstash/conf.d/30-elasticsearch-output.conf >> $LOG
+sudo wget https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-elk-stack/Elk_stack/scripts/10-syslog-filter.conf -O /etc/logstash/conf.d/10-syslog-filter.conf >> $LOG
+sudo wget https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-elk-stack/Elk_stack/scripts/30-elasticsearch-output.conf -O /etc/logstash/conf.d/30-elasticsearch-output.conf >> $LOG
 sudo /opt/logstash/bin/logstash --configtest -f /etc/logstash/conf.d/ >> $LOG
 sudo systemctl restart logstash >> $LOG
 sudo systemctl enable logstash >> $LOG
