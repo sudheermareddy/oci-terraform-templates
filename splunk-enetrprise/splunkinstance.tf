60a9ed18b9cb2073a2c9fdc1763f0cf782ad79cb
sushmithan <sushmithan@sysgain.com>
2017-10-20 12:03:14 +0530

    Update splunkinstance.tf

--- splunk-enetrprise/splunkinstance.tf
+++ splunk-enetrprise/splunkinstance.tf
@@ -8,7 +8,12 @@ resource "oci_core_instance" "splunkvm" {
      image = "${lookup(data.oci_core_images.ImageOCID.images[0], "id")}"
   metadata {
         ssh_authorized_keys = "${var.ssh_public_key}"
-        
+        user_data = "${base64encode(format("%s\n%s %s %s\n",
+           file(var.BootStrapFile),
+           "./enable-password.sh",
+           "${var.admin_username}",
+           "${var.admin_password}"
+        ))}"
   }
  create_vnic_details {
    subnet_id = "${oci_core_subnet.subnet1.id}"
@@ -30,11 +35,9 @@ resource "null_resource" "remote-exec0" {
       inline = [
         "wget https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-splunk-enterprise/splunk-enetrprise/userdata/splunk-install.sh ",
         "chmod 777 splunk-install.sh",
+        "chmod 777 splunk-install.sh",
         "./splunk-install.sh ${var.admin_username} >> remote-exec.log 2>&1"
-
-
       ]
     }
 }
-
 
