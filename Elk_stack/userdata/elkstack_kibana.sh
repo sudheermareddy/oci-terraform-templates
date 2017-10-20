ddfcd2d21c75986dc2dc8964494877c4aaa81e33
komali <komalio@sysgain.com>
2017-10-20 11:55:42 +0530

    Update elkstack_kibana.sh

--- Elk_stack/userdata/elkstack_kibana.sh
+++ Elk_stack/userdata/elkstack_kibana.sh
@@ -14,9 +14,6 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list >> $LOG
 echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
 echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list >> $LOG
-echo "---Configure Repos for Azure Cli 2.0---" >> $LOG
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list >> $LOG
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893 >> $LOG
 
 # Enable silent installation
 echo "---Enable silent installation---"	>> $LOG
@@ -29,7 +26,7 @@ sudo apt-get update
 
 #Installing Packages for ELK Stack
 echo "---Installing Packages for ELK Stack---"	>> $LOG
-sudo apt-get -y install oracle-java8-installer elasticsearch kibana nginx logstash >> $LOG
+sudo apt-get -y install oracle-java8-installer elasticsearch kibana nginx >> $LOG
 
 #Configuring Elasticsearch
 echo "---Configuring Elasticsearch---" >> $LOG
@@ -50,7 +47,8 @@ echo "---Configuring Nginx---" >> $LOG
 sudo sudo -v >> $LOG
 echo "adminuser:`openssl passwd -apr1 'Password@1234'`" | sudo tee -a /etc/nginx/htpasswd.users >> $LOG
 sudo rm /etc/nginx/sites-available/default
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/default -O /etc/nginx/sites-available/default >> $LOG
+sudo wget https://raw.githubusercontent.com/sudheermareddy/test/master/default -O /etc/nginx/sites-available/default
+sudo sed -i 's/example.com/localhost:5601/g' /etc/nginx/sites-available/default
 sudo nginx -t >> $LOG
 sudo systemctl restart nginx >> $LOG
 sudo ufw allow 'Nginx Full' >> $LOG
@@ -63,16 +61,6 @@ sed -i "/\[ v3_ca \]/a subjectAltName = IP: $HOSTIP" /etc/ssl/openssl.cnf >> $LO
 cd /etc/pki/tls >> $LOG
 sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt >> $LOG
 
-#Configuring Logstash
-echo "---Configuring Logstash---" >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/02-beats-input.conf -O /etc/logstash/conf.d/02-beats-input.conf >> $LOG
-sudo ufw allow 5044 >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/10-syslog-filter.conf -O /etc/logstash/conf.d/10-syslog-filter.conf >> $LOG
-sudo wget https://raw.githubusercontent.com/sysgain/MSOSS/master/scripts/30-elasticsearch-output.conf -O /etc/logstash/conf.d/30-elasticsearch-output.conf >> $LOG
-sudo /opt/logstash/bin/logstash --configtest -f /etc/logstash/conf.d/ >> $LOG
-sudo systemctl restart logstash >> $LOG
-sudo systemctl enable logstash >> $LOG
-
 #Configuring Kibana Dashboards
 echo "---Configuring Kibana Dashboards---" >> $LOG
 cd ~
@@ -81,3 +69,17 @@ sudo apt-get install unzip >> $LOG
 unzip beats-dashboards-*.zip >> $LOG
 cd beats-dashboards-* >> $LOG
 ./load.sh >> $LOG
+
+sudo apt-get update
+sudo apt-get install firewalld -y
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=5044/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=5601/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=9200/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --reload
+sudo service elasticsearch start
+sudo service kibana start
+sudo service logstash start
+sudo service nginx start
