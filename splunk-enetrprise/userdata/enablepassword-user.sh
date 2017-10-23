52d2bcb5541d2e9db62d57f962fe53747e22aaf5
sushmithan <sushmithan@sysgain.com>
2017-10-23 11:58:34 +0530

    Delete enablepassword-user.sh

--- splunk-enetrprise/userdata/enablepassword-user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ~ubuntu
-
-curl https://raw.githubusercontent.com/sysgain/oci-terraform-templates/oci-splunk-enterprise/splunk-enetrprise/userdata/enable-password.sh > enable-password.sh
-
-chmod +x enable-password.sh
