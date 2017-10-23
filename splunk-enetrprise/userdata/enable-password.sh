20755eb21c493730fae03b9f6b6434fadfa9f1fd
sushmithan <sushmithan@sysgain.com>
2017-10-23 11:58:15 +0530

    Update enable-password.sh

--- splunk-enetrprise/userdata/enable-password.sh
+++ splunk-enetrprise/userdata/enable-password.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-
+#!/bin/bash
 username=ubuntu
 password=Password@1234
-
+############change username from default username ############
 sudo usermod -l $username ubuntu
 usermod -d /home/$username -m $username
-
-##### Enable 'splunk' user to ssh with a password
+############Enable password authentication############
 echo -e "$password\n$password" | sudo passwd $username
 file="/etc/ssh/sshd_config"
 passwd_auth="yes"
@@ -15,5 +13,3 @@ cat $file \
 > $file.new
 mv $file.new $file
 service sshd restart
-
-
