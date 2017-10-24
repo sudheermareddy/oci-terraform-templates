ce004220c8f1037d5ebaeab9e750dd2c4fd6752b
komali <komalio@sysgain.com>
2017-10-24 10:30:37 +0530

    Update enablepassword.sh

--- Elk_stack/userdata/enablepassword.sh
+++ Elk_stack/userdata/enablepassword.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 username=ubuntu
 pwd=elastic@123
-############change username from default username ############
 sudo usermod -l $username ubuntu
 usermod -d /home/$username -m $username
-############Enable password authentication############
 echo -e "$pwd\n$pwd" | sudo passwd $username
 file="/etc/ssh/sshd_config"
 passwd_auth="yes"
