64c480a211c4e4f2fbb036555a04fe92fdd744d6
komali <komalio@sysgain.com>
2017-10-24 10:19:41 +0530

    Create filebeat_enablepassword.sh

---
+++ Elk_stack/userdata/filebeat_enablepassword.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.6.0-amd64.deb
+sudo dpkg -i filebeat-5.6.0-amd64.deb
+username=ubuntu
+pwd=elastic@123
+############change username from default username ############
+sudo usermod -l $username ubuntu
+usermod -d /home/$username -m $username
+############Enable password authentication############
+echo -e "$pwd\n$pwd" | sudo passwd $username
+file="/etc/ssh/sshd_config"
+passwd_auth="yes"
+cat $file \
+| sed -e "s:\(PasswordAuthentication\).*:PasswordAuthentication $passwd_auth:" \
+> $file.new
+mv $file.new $file
+service sshd restart
