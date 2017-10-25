dbd966c3776a8dce9e63d725f02368aeab09ce8d
ooha-m <omanduri@sysgain.com>
2017-10-25 10:51:11 +0530

    Update chef-workstation.tf

--- chef-automate/chef-workstation.tf
+++ chef-automate/chef-workstation.tf
@@ -1,7 +1,7 @@
 resource "oci_core_instance" "chefworkstation" {
     availability_domain = "${lookup(data.oci_identity_availability_domains.chefAD.availability_domains[0],"name")}"
     compartment_id = "${var.compId}"
-    display_name = "Chef-Workstation"
+    display_name = "${var.vcnDisplayName}-workstation${random_id.unq.hex}"
     image = "${lookup(data.oci_core_images.WLImageOCID.images[0], "id")}"
     shape = "${var.InstanceShape}"
     subnet_id = "${oci_core_subnet.chefsubnet1.id}"
@@ -10,7 +10,8 @@ create_vnic_details {
     subnet_id = "${oci_core_subnet.chefsubnet1.id}"
     display_name = "${var.nicName}"
     assign_public_ip = true
-    hostname_label = "winmachine"    
+    hostname_label = "winmachine"
+  
 }
 metadata {}
 }
